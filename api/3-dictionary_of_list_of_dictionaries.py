c43505cdb9a756a5eae11ec467c12bdf5562c7c4
nathalie <nathalie.metreau@hotmail.fr>
2023-12-18 15:35:16 +0100

    add t3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Records all tasks from all employees, export data in the JSON format.
+"""
+
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+
+	base_url = "https://jsonplaceholder.typicode.com/users"
+	employee_id_response = requests.get(base_url)
+	employee_id_data = employee_id_response.json()
+
+	json_data = {}
+
+	for employee_data in employee_id_data:
+		user_id = employee_data['id']
+		employee_name = employee_data['username']
+		employee_url = f"{base_url}/{user_id}"
+		todos_url = f"{employee_url}/todos"
+
+		try:
+			# Fetch TODO list for the employee
+			todos_response = requests.get(todos_url)
+			todos_data = todos_response.json()
+
+			# Prepare data for json
+			json_data[user_id] = []
+			for todo in todos_data:
+				task_completed_status = todo['completed']
+				task_title = todo['title']
+				json_data[user_id].append({"username": employee_name, "task": task_title, "completed": task_completed_status})
+
+		except requests.exceptions.RequestException as e:
+			print(f"Error: {e}")
+			sys.exit(1)
+
+		# Write to CSV file
+		json_filename = "todo_all_employees.json"
+		with open(json_filename, mode='w') as json_file:
+			json.dump(json_data, json_file)
+
+
