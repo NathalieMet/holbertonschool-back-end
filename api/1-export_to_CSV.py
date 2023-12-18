8700de8229eb88f96688b977d20bc2d6d84a0c35
nathalie <nathalie.metreau@hotmail.fr>
2023-12-18 12:39:46 +0100

    add task 0 API

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    employee_url = f"{base_url}/{employee_id}"
+    todos_url = f"{employee_url}/todos"
+
+    try:
+        # Fetch employee information
+        employee_response = requests.get(employee_url)
+        employee_data = employee_response.json()
+        employee_name = employee_data.get('name')
+
+        # Fetch TODO list for the employee
+        todos_response = requests.get(todos_url)
+        todos_data = todos_response.json()
+
+        # Calculate progress
+        total_tasks = len(todos_data)
+        completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+
+        # Display progress information
+        print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+
+        # Display titles of completed tasks
+        completed_task_titles = [todo['title'] for todo in todos_data if todo['completed']]
+        for title in completed_task_titles:
+            print(f"\t{title}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+        
