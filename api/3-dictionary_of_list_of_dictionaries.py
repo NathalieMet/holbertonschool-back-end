99a4ba93d9b1bb0f3b7ce79a8665438e4f89d0a7
nathalie <nathalie.metreau@hotmail.fr>
2023-12-20 20:29:06 +0100

    correct pycodesyle on t3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,40 +8,41 @@ import requests
 import sys
 
 if __name__ == "__main__":
-	"""Actions to be performed only when the script
+    """Actions to be performed only when the script
     is run directly"""
 
-	base_url = "https://jsonplaceholder.typicode.com/users"
-	employee_id_response = requests.get(base_url)
-	employee_id_data = employee_id_response.json()
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    employee_id_response = requests.get(base_url)
+    employee_id_data = employee_id_response.json()
 
-	json_data = {}
+    json_data = {}
 
-	for employee_data in employee_id_data:
-		user_id = employee_data['id']
-		employee_name = employee_data['username']
-		employee_url = f"{base_url}/{user_id}"
-		todos_url = f"{employee_url}/todos"
+    for employee_data in employee_id_data:
+        user_id = employee_data['id']
+        employee_name = employee_data['username']
+        employee_url = f"{base_url}/{user_id}"
+        todos_url = f"{employee_url}/todos"
 
-		try:
-			# Fetch TODO list for the employee
-			todos_response = requests.get(todos_url)
-			todos_data = todos_response.json()
+        try:
+            # Fetch TODO list for the employee
+            todos_response = requests.get(todos_url)
+            todos_data = todos_response.json()
 
-			# Prepare data for json
-			json_data[user_id] = []
-			for todo in todos_data:
-				task_completed_status = todo['completed']
-				task_title = todo['title']
-				json_data[user_id].append({"username": employee_name,
-								"task": task_title,
-								"completed": task_completed_status})
+            # Prepare data for json
+            json_data[user_id] = []
+            for todo in todos_data:
+                task_completed_status = todo['completed']
+                task_title = todo['title']
+                json_data[user_id].append({
+                    "username": employee_name,
+                    "task": task_title,
+                    "completed": task_completed_status})
 
-		except requests.exceptions.RequestException as e:
-			print(f"Error: {e}")
-			sys.exit(1)
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            sys.exit(1)
 
-		# Write to CSV file
-		json_filename = "todo_all_employees.json"
-		with open(json_filename, mode='w') as json_file:
-			json.dump(json_data, json_file)
+        # Write to CSV file
+        json_filename = "todo_all_employees.json"
+        with open(json_filename, mode='w') as json_file:
+            json.dump(json_data, json_file)
